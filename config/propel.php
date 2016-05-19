73134184dc898a19ef0cecdbe52f068237a9d8cd
Snake231088 <solidboss@gmail.com>
2016-05-19 18:09:48 +0200

    Master propel.php
    Revert master propel.php for fix
    No connection defined for database "mysql". Did you forget to define a connection or is it wrong written?

--- config/propel.php
+++ config/propel.php
@@ -1,10 +1,9 @@
 <?php
-
 return [
     'propel' => [
         'general' => [
             # The name of your project.
-            'project' => 'My App',
+            'project' => 'MyApp',
             'version' => '1.0',
         ],
         'exclude_tables' => [
@@ -28,7 +27,7 @@ return [
             'phpDir' => app_path('Models'),
 
             # The directory where Propel should output the compiled runtime configuration.
-            'phpConfDir' => config_path('propel'),
+            'phpConfDir' => base_path('config/propel'),
 
             # The directory where Propel should output the generated migrations.
             'migrationDir' => base_path('database/migrations'),
@@ -43,36 +42,45 @@ return [
         'database' => [
             # All database sources
             /***** We use data from database.php, please do not change this if you do not understand this code.  *****/
-            'connections' => [
-                'default' => [
-                    'adapter' => 'mysql',
-                    # Connection class. One of the Propel\Runtime\Connection classes
-                    'classname' => (env('APP_DEBUG', false) ? \Propel\Runtime\Connection\ProfilerConnectionWrapper::class : \Propel\Runtime\Connection\ConnectionWrapper::class),
-                    # The PDO dsn
-                    'dsn' => 'mysql:host='.env('DB_HOST', 'localhost').';port=3306;dbname='.env('DB_DATABASE', 'forge'),
-                    'user' => env('DB_USERNAME', 'forge'),
-                    'password' => env('DB_PASSWORD', ''),
-                    # Driver options. See http' => '//www.php.net/manual/en/pdo.construct.php
-                    # options must be passed to the contructor of the connection object
-                    'options' => [],
-                    # See http://www.php.net/manual/en/pdo.getattribute.php
-                    # Attributes are set via `setAttribute()` method, after the connection object is created
-                    'attributes' => [],
-                    #Propel specific settings
-                    'settings' => [
-                        'charset' => 'utf8',
-                        #Array of queries to run when the database connection is initialized
-                        'query' => [
-                            'SET NAMES utf8 COLLATE utf8_unicode_ci, COLLATION_CONNECTION = utf8_unicode_ci, COLLATION_DATABASE = utf8_unicode_ci, COLLATION_SERVER = utf8_unicode_ci'
+            'connections' => array_map(
+                function($item)
+                {
+                    return [
+                        'adapter' => $item['driver'],
+                        # Connection class. One of the Propel\Runtime\Connection classes
+                        'classname' => (app('config')->get('app.debug') ? \Propel\Runtime\Connection\ProfilerConnectionWrapper::class : \Propel\Runtime\Connection\ConnectionWrapper::class),
+                        # The PDO dsn
+                        'dsn' => $item['driver'] . ':host=' . $item['host'] . ';port=' . (empty($item['port']) ? '3306' : $item['port']) . ';dbname=' . $item['database'],
+                        'user' => $item['username'],
+                        'password' => $item['password'],
+                        # Driver options. See http' => '//www.php.net/manual/en/pdo.construct.php
+                        # options must be passed to the contructor of the connection object
+                        'options' => [],
+                        # See http://www.php.net/manual/en/pdo.getattribute.php
+                        # Attributes are set via `setAttribute()` method, after the connection object is created
+                        'attributes' => [],
+                        #Propel specific settings
+                        'settings' => [
+                            'charset' => $item['charset'],
+                            #Array of queries to run when the database connection is initialized
+                            'query' => [
+                                'SET NAMES utf8 COLLATE utf8_unicode_ci, COLLATION_CONNECTION = utf8_unicode_ci, COLLATION_DATABASE = utf8_unicode_ci, COLLATION_SERVER = utf8_unicode_ci'
+                            ],
                         ],
-                    ],
-                    'slaves' => [
-                        //[
-                        //    'dsn' => 'mysq:host=slave-host-1;dbname=bookstore',
-                        //],
-                    ],
-                ],
-            ],
+                        'slaves' => [
+                            //[
+                            //    'dsn' => 'mysq:host=slave-host-1;dbname=bookstore',
+                            //],
+                        ],
+                    ];
+                },
+                array_filter(
+                    app('config')->get('database.connections'),
+                    function($item) {
+                        return in_array($item['driver'], ['pgsql', 'mysql']);
+                    }
+                )
+            ),
 
             ## Specific adapter settings
             'adapters' => [
@@ -116,7 +124,7 @@ return [
         ## Reverse settings
         'reverse' => [
             # The connection to use to reverse the database
-            'connection' => 'default',
+            'connection' => app('config')->get('database.default'),
 
             # Reverse parser class can be different from migration one
             # If you leave this property blank, Propel looks for an appropriate parser class, based on platform: i.e.
@@ -126,11 +134,11 @@ return [
 
         ## Runtime settings ##
         'runtime' => [
-            'defaultConnection' => 'default',
+            'defaultConnection' => app('config')->get('database.default'),
             # Datasources as defined in database.connections
             # This section affects config:convert command
             'connections' => [
-                'default',
+                app('config')->get('database.default'),
             ],
             ## Log and loggers definitions ##
             # For `type` and `level` options see Monolog documentation https://github.com/Seldaek/monolog
@@ -163,39 +171,33 @@ return [
                 'classname' => \Propel\Runtime\Util\Profiler::class,
                 'slowTreshold' => 0.1,
                 'details' => [
-                    /*
                     'time' => [
-                        'name'      => 'Time',
                         'precision' => 3,
                         'pad' => 8,
                     ],
-                    'mem' => [
-                        'name'      => 'Memory',
+                    'memory' => [
                         'precision' => 3,
                         'pad' => 8,
                     ],
                     'memDelta' => [
-                        'name'      => 'Memory Delta',
                         'precision' => 3,
                         'pad' => 8,
                     ],
                     'memPeak' => [
-                        'name'      => 'Memory Peak',
                         'precision' => 3,
                         'pad' => 8,
                     ],
-                    */
                 ],
-                'innerGlue' => ': ',
-                'outerGlue' => ' | ',
+                'innerGlue' => ':',
+                'outerGlue' => '|',
             ],
         ],
         ## Generator settings ##
         'generator' => [
-            'defaultConnection' => 'default',
+            'defaultConnection' => app('config')->get('database.default'),
             # Datasources as defined in database.connections
             'connections' => [
-                'default',
+                app('config')->get('database.default'),
             ],
 
             # Add a prefix to all the table names in the database.
@@ -275,7 +277,7 @@ return [
                 'addGenericMutators' => true,
                 'emulateForeignKeyConstraints' => false,
                 'addClassLevelComment' => true,
-                'defaultKeyType' => 'phpName',
+                'defaultKeyType' => 'fieldName',
                 'addSaveMethod' => true,
                 'namespaceMap' => 'Map',
 
